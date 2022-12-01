21f9ec246da7b34903b7c9988c81cc2e96c10c57
Yarin181 <yarinsason@gmail.com>
2022-12-01 13:04:45 +0200

    adapting client

--- tcp_client.py
+++ tcp_client.py
@@ -1,7 +1,10 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 12345))
-s.send(b'hello')
+s.connect(('10.0.2.4', 12349))
+s.send(b'yarin')
 data = s.recv(100)
-print("Server sent: ", data)
+print("Server sent: ", data.decode())
+s.send(b'318229143')
+data = s.recv(100)
+print("Server sent: ", data.decode())
 s.close()
