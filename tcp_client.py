68de6cc5793aa22866fb8026a61b77ec214b9d25
Yarin181 <yarinsason@gmail.com>
2022-12-01 19:40:59 +0200

    client update

--- tcp_client.py
+++ tcp_client.py
@@ -1,6 +1,6 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('10.0.2.4', 12349))
+s.connect(('10.0.2.4', 12344))
 s.send(b'yarin')
 data = s.recv(100)
 print("Server sent: ", data.decode())
