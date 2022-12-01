25643f382326d43dfa5dbc2546ecd8b9bd852533
yarin181 <yarinsason@gmail.com>
2022-12-01 12:03:54 +0200

    adapting server

--- tcp_server.py
+++ tcp_server.py
@@ -1,13 +1,16 @@
 import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('', 12345))
+server.bind(('', 12341))
 server.listen(5)
 
 while True:
     client_socket, client_address = server.accept()
     print('Connection from: ', client_address)
     data = client_socket.recv(100)
-    print('Received: ', data)
+    print('Received: ', data.decode())
+    client_socket.send(data.upper())
+    data = client_socket.recv(100)
+    print('Received: ', data.decode())
     client_socket.send(data.upper())
     client_socket.close()
     print('Client disconnected')
