b110dbb5c6b8ecf579ff1f6beae075f3ccffb9d1
yarin181 <yarinsason@gmail.com>
2022-12-02 15:49:19 +0200

    adding support to file not found case

--- server.py
+++ server.py
@@ -76,22 +76,38 @@ def format_message_to_the_client(status_number,status,connection_status,content_
 def main():
     #print(os.listdir("files/files"))
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('', 12342))
+    server.bind(('', 12345))
     server.listen(5)
-    
+
+
     while True:
         client_socket, client_address = server.accept()
-
+        #if the client won't send date within a second a timeoutError will raise.
+        client_socket.settimeout(1)
         while True:
-            data = (client_socket.recv(1024).decode()).split("\r\n")
+            try:
+                data = (client_socket.recv(1024).decode()).split("\r\n")
+            except TimeoutError:
+                client_socket.close()
+                #debug message
+                print('Client disconnected(timeout)')
+                break
+            if (len(data) == 0):
+                client_socket.close()
+                break
             clientData  = ClientData(data)
 
+            #debug messages
             print("path:", "_",clientData._path,"_")
             print("connection:","_",clientData._Connection,"_")
-            f = open("files"+clientData._path,"rb")
-            client_socket.send((format_message_to_the_client(200,"OK",clientData._Connection,f.__sizeof__)).encode())
-            client_socket.sendfile(f)
-
+            print(os.path.isfile("files"+clientData._path))
+            if (os.path.isfile("files"+clientData._path)):
+                f = open("files"+clientData._path,"rb")
+                client_socket.send((format_message_to_the_client(200,"OK",clientData._Connection,f.__sizeof__)).encode())
+                client_socket.sendfile(f)
+            else:
+                client_socket.send((format_message_to_the_client(404,"Not Found","close",0)).encode())
+                client_socket.close()
 
             if(clientData == "close"):
                 client_socket.close()
