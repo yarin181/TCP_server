811c6bd9e83458c294d4a989f75afbdac8382912
yarin181 <yarinsason@gmail.com>
2022-12-02 11:39:20 +0200

    adding utility function and change the main loop logic

--- server.py
+++ server.py
@@ -1,16 +1,99 @@
 import socket
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('', 12345))
-server.listen(5)
+import sys 
+import os
 
-while True:
-    client_socket, client_address = server.accept()
-    print('Connection from: ', client_address)
-    data = client_socket.recv(100)
-    print('Received: ', data.decode())
-    client_socket.send(data.upper())
-    data = client_socket.recv(100)
-    print('Received: ', data.decode())
-    client_socket.send(data.upper())
-    client_socket.close()
-    print('Client disconnected')
+class ClientData:
+    """_summary_ 
+    this class stands for the relevant data that sends from the user which is it , the path of the file 
+    and the connection state.
+    """
+    def __init__(self,data_array):
+        #on this location theres the path of the file.
+        self._path = ClientData.find_path(data_array)
+        #gets the connection state from find_connection func.
+        self._Connection = ClientData.find_connection(data_array)
+
+    @staticmethod    
+    def find_connection(data_array):
+        """_summary_
+            this function get data array and return the sate of the connection 
+            label.
+        Args:
+            data_array (_type_): string array
+            array of the client data.
+        Returns:
+            _type_: string
+            the connection state 
+
+        """
+        for data in data_array:
+            #only if the label length is grater than 12 it could be the connection
+            #label and than ensure its the right one by name.
+            #finlay return the connection state string.
+            if (len(data) > 12 and data[:10] == "Connection"):
+                return data[12:]
+    
+    @staticmethod
+    def find_path(data_array):
+        if (data_array[0])[4:-9] == "/":
+            return "/index.html"
+        else:
+            return (data_array[0])[6:-10]
+
+def format_message_to_the_client(status_number,status,connection_status,content_length):
+    """_summary_
+
+        This function arrange the inserted data in a http format for sending to the user.
+    Args:
+        status_number int:
+        200 - the file is found
+        404 - file not found 
+        301 - the client asked for file in the name of "redirected"
+    
+        status  string :
+        OK - file found .
+        Not Found - the file not founded.
+        Moved Permanently - the client asked for file in the name of "redirected"
+
+        connection_status string: 
+        "keep-alive" or "close" 
+
+        content_length int: 
+        the content length
+
+
+    return a appropriate string.
+    """
+
+    if (status_number == 200):
+        return "HTTP/1.1 {} {} \r\nConnection: {}\r\nContent-Length: {}\r\n\r\n".format(status_number,status,connection_status,content_length)
+    if (status_number == 404):
+        return "HTTP/1.1 {} {} \r\nConnection: {}\r\n".format(status_number,status,connection_status)
+    if (status_number == 301):
+        return "HTTP/1.1 {} {} \r\nConnection: {}\r\nLocation: /result.html\r\n".format(status_number,status,connection_status)
+
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('', 12340))
+    server.listen(5)
+
+    while True:
+        client_socket, client_address = server.accept()
+
+        while True:
+            data = (client_socket.recv(1024).decode()).split("\r\n")
+            clientData  = ClientData(data)
+
+            print("path:", "_",clientData._path,"_")
+            print("connection:","_",clientData._Connection,"_")
+
+
+
+            if(clientData == "close"):
+                client_socket.close()
+                print('Client disconnected')
+
+
+if __name__ == "__main__":
+    main()
