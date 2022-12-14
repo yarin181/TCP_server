8cd9d25d18aa9dba37473c1c7612e0e280145f6e
orelbenshamai <79166869+orelbenshamai@users.noreply.github.com>
2022-12-14 15:30:14 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
 import socket
-import sys 
+import sys
 import os
-import time 
-BUFFER_SIZE = 1024
-REQUEST_SEPARATOR = "/r/n/r/n"
+
+BUFFER_SIZE = 4096
+REQUEST_SEPARATOR = "\r\n\r\n"
 
 """ _summary_ 
     this class stands for the relevant data that sends from the user which is it , the path of the file 
     and the connection state.
 """
+
+
 class ClientData:
-    def __init__(self,data_array):
-        #on this location theres the path of the file.
+    def __init__(self, data_array):
+        # on this location theres the path of the file.
         self._path = ClientData.find_path(data_array)
-        #gets the connection state from find_connection func.
+        # gets the connection state from find_connection func.
         self._connection = ClientData.find_connection(data_array)
 
     """_summary_
@@ -26,21 +28,23 @@ class ClientData:
             _type_: string
             the connection state 
     """
-    @staticmethod    
+
+    @staticmethod
     def find_connection(data_array):
         for data in data_array:
-            #only if the label length is grater than 12 it could be the connection
-            #label and than ensure its the right one by name.
-            #finlay return the connection state string.
+            # only if the label length is grater than 12 it could be the connection
+            # label and than ensure its the right one by name.
+            # finlay return the connection state string.
             if (len(data) > 12 and data[:10] == "Connection"):
                 return data[12:]
-    
+
     @staticmethod
     def find_path(data_array):
         if (data_array[0])[4:-9] == "/":
             return "/index.html"
         else:
             return (data_array[0])[4:-9]
+
 
 """_summary_
         This function arrange the inserted data in a http format for sending to the user.
@@ -49,7 +53,7 @@ class ClientData:
         200 - the file is found
         404 - file not found 
         301 - the client asked for file in the name of "redirected"
-    
+
         status  string :
         OK - file found .
         Not Found - the file not founded.
@@ -63,17 +67,21 @@ class ClientData:
 
     returns an appropriate string.
 """
-def format_message_to_the_client(status_number,status,connection_status,content_length):
+
+
+def format_message_to_the_client(status_number, status, connection_status, content_length):
     if (status_number == 200):
-        return "HTTP/1.1 {} {} \r\nConnection: {}\r\nContent-Length: {}\r\n\r\n".format(status_number,status,connection_status,content_length)
+        return "HTTP/1.1 {} {} \r\nConnection: {}\r\nContent-Length: {}\r\n\r\n".format(status_number, status,
+                                                                                        connection_status,
+                                                                                        content_length)
     if (status_number == 404):
-        return "HTTP/1.1 {} {} \r\nConnection: {}\r\n\r\n".format(status_number,status,connection_status)
+        return "HTTP/1.1 {} {} \r\nConnection: {}\r\n\r\n".format(status_number, status, connection_status)
     if (status_number == 301):
-        return "HTTP/1.1 {} {} \r\nConnection: {}\r\nLocation: {}\r\n\r\n".format(status_number,status,connection_status,content_length)
+        return "HTTP/1.1 {} {} \r\nConnection: {}\r\nLocation: {}\r\n\r\n".format(status_number, status,
+                                                                                  connection_status, content_length)
 
 
 def main():
-    
     if len(sys.argv) != 2:
         server.close()
         sys.exit(1)
@@ -84,51 +92,55 @@ def main():
 
     while True:
         client_socket, client_address = server.accept()
-        #if the client won't send date within a second a timeoutError will raise.
-
-        while True:
+        total_data = ""
+        current_client_flag = True
+        while current_client_flag:
             client_socket.settimeout(1)
             try:
-                time.sleep(5)
-                data = client_socket.recv(BUFFER_SIZE).decode()
-                splitted_data = data.split("\r\n")
+                received_data = client_socket.recv(BUFFER_SIZE).decode()
 
-                if (len(data) == 0):
+                if (len(received_data) == 0):
                     client_socket.close()
+                    current_client_flag = False
                     break
-                
-                # print the request
-                print(data)
+                total_data += received_data
 
-                clientData  = ClientData(splitted_data)
-                isFile = os.path.isfile("files"+clientData._path)
-                if(clientData._path == "/redirect"):
-                    client_socket.send((format_message_to_the_client(301,"Moved Permanently","close","/result.html")).encode())
-                    client_socket.close()
-                    break
-                elif isFile:
-                    filepath = "files" + clientData._path
-                    f = open("files"+clientData._path,"rb")
-                    binaryFile = f.read()
-                    f.close()
-                    dataToSend = format_message_to_the_client(200,"OK",clientData._connection,os.stat(filepath).st_size)
-                    client_socket.send(dataToSend.encode() + binaryFile)
-                else:
-                    client_socket.send((format_message_to_the_client(404,"Not Found","close",0)).encode())
-                    client_socket.close()
-                    break
-                if(clientData._connection == "close"):
-                    # debug message
-                    #print('Client disconnected')
-                    client_socket.close()
-                    break
+                while REQUEST_SEPARATOR in total_data:
+                    partitioned_data = total_data.partition(REQUEST_SEPARATOR)
+                    current_request = partitioned_data[0] + partitioned_data[1]
+                    total_data = partitioned_data[2]
 
+                    print(current_request)
+
+                    clientData = ClientData(current_request.split("\r\n"))
+                    isFile = os.path.isfile("files" + clientData._path)
+                    if (clientData._path == "/redirect"):
+                        client_socket.send(
+                            (format_message_to_the_client(301, "Moved Permanently", "close", "/result.html")).encode())
+                        client_socket.close()
+                        current_client_flag = False
+                        break
+                    elif isFile:
+                        filepath = "files" + clientData._path
+                        f = open("files" + clientData._path, "rb")
+                        binaryFile = f.read()
+                        f.close()
+                        dataToSend = format_message_to_the_client(200, "OK", clientData._connection,
+                                                                  os.stat(filepath).st_size)
+                        client_socket.send(dataToSend.encode() + binaryFile)
+                    else:
+                        client_socket.send((format_message_to_the_client(404, "Not Found", "close", 0)).encode())
+                        client_socket.close()
+                        current_client_flag = False
+                        break
+                    if (clientData._connection == "close"):
+                        client_socket.close()
+                        current_client_flag = False
+                        break
             except TimeoutError:
-                #debug message
-                #print('Client disconnected(timeout)', client_address)
                 client_socket.close()
                 break
-            
+
 
 if __name__ == "__main__":
     main()
